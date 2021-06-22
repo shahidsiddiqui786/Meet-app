9e8e417841343737da3e55e2a325ec1640032119
shahidsiddiqui786 <shahid99846434@gmail.com>
2021-06-22 23:36:36 +0530

    dark mode added

--- src/components/Input/EmojiGrid/EmojiGrid.js
+++ src/components/Input/EmojiGrid/EmojiGrid.js
@@ -7,17 +7,13 @@ let emoji = require("node-emoji");
 
 
 const EmojiGrid = ({  addEmojiToInput }) => {
-  // emoji.search('').map((item) => {
-  //     return  console.log(item.emoji)
-  //   })
-    
-
+  
   return (
-            <div id="emoji-grid">
-                {emoji.search('').map((emojiElement) => {
-                return <Emoji key={emojiElement.key}  addEmojiToInput={addEmojiToInput} emoji={emojiElement.emoji}></Emoji>
-                } )}
-            </div>
+    <div id="emoji-grid">
+        {emoji.search('').map((emojiElement) => {
+        return <Emoji key={emojiElement.key}  addEmojiToInput={addEmojiToInput} emoji={emojiElement.emoji}></Emoji>
+        } )}
+    </div>
 )
 
 }
