b92febe139a9302808c42c855b356fbf369d7b8f
shahidsiddiqui786 <shahid99846434@gmail.com>
2021-06-26 08:45:01 +0530

    manual resolved issues

--- src/components/Messages/Message/Message.js
+++ src/components/Messages/Message/Message.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useTheme } from '../../Provider/ThemeContext'
 import './Message.css';
 
 
@@ -13,12 +13,33 @@ const Message = ({ message: { text, user, media }, name }) => {
     isSentByCurrentUser = true;
   }
 
+  const darkTheme = useTheme()
+
+  const msgBoxBlueStyle = {
+    backgroundColor: darkTheme ? '#2858a5' : '#7ecfea',
+    color: darkTheme ? '#fff' : '#000036'
+  }
+
+  const msgBoxLightStyle = {
+    backgroundColor: darkTheme ? '#75838a' : '#5fe498',
+    color: darkTheme ? '#fff' : '#000060'
+  }
+
+  const sentTextStyle = {
+    color: darkTheme ? '#857b7b' : '#030317'
+  }
+
+  // const bgBlueStyle = {
+  //   backgroundColor: darkTheme ? '#03304a' : '#2858a5',
+  //   color: darkTheme #857b7b ? '#00D4D4' : '#222'
+  // }
+
 
   return (
     isSentByCurrentUser
       ? (
         <div className="messageContainer justifyEnd">
-          <div className="messageBox backgroundBlue">
+          <div style={msgBoxBlueStyle} className="messageBox">
             { media && 
               <img className="image" src={text} alt={text} /> }
             { !media && 
@@ -28,16 +49,16 @@ const Message = ({ message: { text, user, media }, name }) => {
         )
         : (
           <div className="messageContainer justifyStart">
-            <div className="messageBox backgroundLight">
+            <div style={msgBoxLightStyle} className="messageBox">
               { media && 
                <img className="image" src={text} alt={text} /> }
             { !media && 
               <p className="messageText">{text} </p> }
             </div>
-            <p className="sentText pl-10 ">{user}</p>
+            <p style={sentTextStyle} className="sentText pl-10 ">{user}</p>
           </div>
         )
   );
 }
 
-export default Message;
+export default Message
--- src/components/UserContainer/UserContainer.js
+++ src/components/UserContainer/UserContainer.js
@@ -1,34 +1,53 @@
 import React from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
-import './TextContainer.css';
+import './UserContainer.css';
+import { useTheme } from '../Provider/ThemeContext'
 
-const TextContainer = ({  users }) => {
-        return (
-          users
-            ? (
-            <ScrollToBottom >
-              <div className="user-list">
-                <div className="activeContainer">
-                    {users.map(({name}) => (
-                      <div key={name} className="activeItem">
-                        <div className="user-profile">
-                          <div key={name.charAt(0)} className="photo-profile">
-                            <strong>{name.charAt(0)}</strong>
-                          </div>
-                          <div key={name} className="user-name">
-                          {name}
-                          </div>
-                        </div>
-                        <hr></hr>
-                      </div>
-                    ))}
+const UserContainer = ({  users }) => {
+
+  const darkTheme = useTheme()
+
+  const profileStyle = {
+    backgroundColor: darkTheme ? '#03304a' : '#1b76aa',
+    color: darkTheme ? '#00D4D4' : '#222'
+  }
+
+  const userStyle = {
+    backgroundColor: darkTheme ? '#03304a' : '#1b76aa',
+    color: darkTheme ? '#00D4D4' : '#222'
+  } 
+  const activeItemStyle = {
+    backgroundColor: darkTheme ? 'rgb(6, 3, 51)' : '#2f52e6',
+    color: darkTheme ? '#00D4D4' : '#222'
+  }    
+
+return (
+  users
+    ? (
+    <ScrollToBottom >
+      <div style={userStyle} className="user-list">
+        <div className="activeContainer">
+            {users.map(({name}) => (
+              <div style={activeItemStyle} key={name} className="activeItem">
+                <div className="user-profile">
+                  <div style={profileStyle} key={name.charAt(0)} className="photo-profile">
+                    <strong>{name.charAt(0)}</strong>
+                  </div>
+                  <div key={name} className="user-name">
+                  {name.substring(0,8)}
+                  </div>
                 </div>
+                <hr></hr>
               </div>
-            </ScrollToBottom>
-            )
-            : null
-          )
-      }
+            ))}
+        </div>
+      </div>
+    </ScrollToBottom>
+    )
+    : 
+    <h2>Hey , No One here!</h2>
+  )
+}
 
 
-export default TextContainer;
+export default UserContainer;
